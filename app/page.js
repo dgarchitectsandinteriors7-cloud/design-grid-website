df0d4c60010ea11343ce4b8ab966cfb11eb7defe
Design Grid <dgarchitectsandinteriors7@gmail.com>
2026-05-18 17:07:54 +0530

    Fixed JSX build

--- app/page.js
+++ app/page.js
@@ -191,7 +191,7 @@ export default function DesignGridWebsite() {
             </p>
 
             <h3 className="text-5xl font-bold mb-6">
-              Luxury Interior Gallery
+              Luxury Interior Gallerys
             </h3>
 
             <p className="text-gray-400 max-w-3xl mx-auto text-lg">
